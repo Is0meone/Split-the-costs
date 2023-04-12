0c921359b4615f3cb148702a8e65c269f776c233
jblizniu <jakub.blizniuk.stud@pw.edu.pl>
2023-04-12 15:37:38 +0200

    ci: add autoamted testing

--- api/src/main/java/pl/edu/pw/api/ApiApplication.java
+++ api/src/main/java/pl/edu/pw/api/ApiApplication.java
@@ -2,7 +2,6 @@ package pl.edu.pw.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +13,6 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-	@Bean("securityFilterChain")
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests(
