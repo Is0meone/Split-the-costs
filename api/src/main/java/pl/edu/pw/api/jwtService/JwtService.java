fb5f1ed1c3aa2c8cc5e09e66a08530ad94981db9
Bartłomiej Roszczyk <01169074@pw.edu.pl>
2023-05-11 01:08:24 +0200

    JWT Validation

--- api/src/main/java/pl/edu/pw/api/jwtService/JwtService.java
+++ api/src/main/java/pl/edu/pw/api/jwtService/JwtService.java
@@ -1,15 +1,19 @@
 package pl.edu.pw.api.jwtService;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import pl.edu.pw.api.auth.AuthController;
+import pl.edu.pw.api.auth.dto.LoginDTO;
 
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtService {
@@ -33,5 +37,46 @@ public class JwtService {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
 
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String token) {
+        try {
+            return Jwts
+                    .parserBuilder()
+                    .setSigningKey(getSignKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch(Exception exception) {
+            System.out.println(exception);
+        }
+        return null;
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public Boolean validateToken(String token, String name) {
+        final String username = extractUsername(token);
+        return (username.equals(name) && !isTokenExpired(token));
+    }
+    public static void main(String[] args) {
+        JwtService j = new JwtService();
+        String token = j.generateToken("imie");
+        System.out.println(token);
+        System.out.println(j.extractAllClaims(token));
+        System.out.println(j.extractUsername(token));
+        System.out.println(j.validateToken(token,"imie"));
+    }
 }
