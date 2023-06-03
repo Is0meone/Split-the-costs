fd5539231e81dcd7e6202d45c1308b4c66da4bf7
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-06-03 21:19:40 +0200

    Wstepne prace nad gui, dokladniej login, register i mainpage

--- app/src/main/java/pl/edu/pw/app/ExpenseSplitterApplication.java
+++ app/src/main/java/pl/edu/pw/app/ExpenseSplitterApplication.java
@@ -11,8 +11,8 @@ public class ExpenseSplitterApplication extends Application {
 	@Override
 	public void start(Stage stage) throws IOException {
 		FXMLLoader fxmlLoader = new FXMLLoader(ExpenseSplitterApplication.class.getResource("login-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+		Scene scene = new Scene(fxmlLoader.load());
+//		stage.setTitle("Hello!");
 		stage.setScene(scene);
 		stage.show();
 	}
--- app/src/main/java/pl/edu/pw/app/LoginController.java
+++ app/src/main/java/pl/edu/pw/app/LoginController.java
@@ -1,4 +1,33 @@
 package pl.edu.pw.app;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goToRegisterView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("register-view.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToMainPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-page.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
