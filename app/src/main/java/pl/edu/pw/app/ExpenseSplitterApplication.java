ca7f5ac8eaa8185aba72828a51dd6e4f5d4deae5
bdmitruk <01178272@pw.edu.pl>
2023-06-09 22:48:44 +0200

    Improved the main page by adding a user balance display. (Further enhancements will be made once obligations are handled)

--- app/src/main/java/pl/edu/pw/app/ExpenseSplitterApplication.java
+++ app/src/main/java/pl/edu/pw/app/ExpenseSplitterApplication.java
@@ -5,19 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
 
 public class ExpenseSplitterApplication extends Application {
-    public static void main(String[] args) {
-        launch();
-    }
+	@Override
+	public void start(Stage stage) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(ExpenseSplitterApplication.class.getResource("login-view.fxml"));
+		Scene scene = new Scene(fxmlLoader.load());
+		stage.setScene(scene);
+		stage.sizeToScene();
+		stage.show();
+	}
 
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ExpenseSplitterApplication.class.getResource("login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.show();
-    }
+	public static void main(String[] args) {
+		launch();
+	}
 }
--- app/src/main/java/pl/edu/pw/app/MainPageController.java
+++ app/src/main/java/pl/edu/pw/app/MainPageController.java
@@ -1,5 +1,9 @@
 package pl.edu.pw.app;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +14,10 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -38,8 +45,101 @@ public class MainPageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO
-        // initialize friendsListView, userBalance, userGreet, userId
+        // TODO: initialize friendsListView, userGreet, userId
+        try {
+            double balance = getUserBalance(userId.getText());
+            updateUserBalance(balance);
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception appropriately
+        }
+    }
+
+    private double getUserBalance(String userId) throws IOException {
+        return getUserLoans(userId) - getUserDebts(userId);
+    }
+
+    private double getUserDebts(String userId) throws IOException {
+        String url = "http://localhost:8090/obligations/user/" + userId + "/debts";
+
+        URL address = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) address.openConnection();
+        con.setRequestMethod("GET");
+
+        // Get response
+        int responseCode = con.getResponseCode();
+        if (responseCode != 200) {
+            // Handle the error response appropriately
+            return 0.0;
+        }
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            String responseBody = response.toString();
+            // Parse the JSON response to get the user balance
+            double debt = parseUserBalanceFromJson(responseBody);
+            return debt;
+        }
+    }
+
+    private double getUserLoans(String userId) throws IOException {
+        String url = "http://localhost:8090/obligations/user/" + userId + "/credits";
+
+
+        URL address = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) address.openConnection();
+        con.setRequestMethod("GET");
+
+        // Get response
+        int responseCode = con.getResponseCode();
+        if (responseCode != 200) {
+            // Handle the error response appropriately
+            return 0.0;
+        }
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            String responseBody = response.toString();
+            // Parse the JSON response to get the user balance
+            double loan = parseUserBalanceFromJson(responseBody);
+            return loan;
+        }
+    }
+
+    private double parseUserBalanceFromJson(String json) {
+        JsonObject jsonObject = JsonParser.parseString(json).getAsJsonObject();
+        JsonArray debtsArray = jsonObject.getAsJsonArray("debts");
+        double balance = 0.0;
+        for (JsonElement element : debtsArray) {
+            JsonObject debtObject = element.getAsJsonObject();
+            double amount = debtObject.get("amount").getAsDouble();
+            String creditorId = debtObject.get("creditor").getAsJsonObject().get("id").getAsString();
+            String debtorId = debtObject.get("debtor").getAsJsonObject().get("id").getAsString();
+
+            // Calculate the balance based on the creditor and debtor IDs
+            // Adjust the balance calculation logic according to your requirements
+            if (creditorId.equals(userId)) {
+                balance -= amount;
+            } else if (debtorId.equals(userId)) {
+                balance += amount;
+            }
+        }
+        return balance;
+    }
+
+
+    private void updateUserBalance(double balance) {
+        userBalance.setText(String.valueOf(balance));
     }
 
     public void setUserPane(AnchorPane userPane) {
@@ -96,20 +196,5 @@ public class MainPageController implements Initializable {
     public void setToken(String token) {
         this.token = token;
     }
-
-
-
-    /*@FXML
-    private void handleExpenseViewAction(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("add-expense-view.fxml"));
-        AnchorPane splitExpenseView = loader.load();
-        SplitExpenseController splitExpenseController = loader.getController();
-
-        // Przekazanie informacji/parametrów do kontrolera sceny "split-expense-view" (opcjonalne)
-        // splitExpenseController.setSomeData(someData);
-
-        mainPane.getChildren().setAll(splitExpenseView);
-    }*/
-
 
 }
