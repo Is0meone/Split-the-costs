5ddf424b292e09331a1f4e60495d301c02a6efcb
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-06-14 20:36:36 +0200

    poczatki split expense

--- app/src/main/java/pl/edu/pw/app/SplitExpenseController.java
+++ app/src/main/java/pl/edu/pw/app/SplitExpenseController.java
@@ -1,13 +1,36 @@
 package pl.edu.pw.app;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SplitExpenseController {
+public class SplitExpenseController implements Initializable {
+    @FXML
+    private ListView<String> friendlist;
+    @FXML
+    private TextField expenseName;
+    @FXML
+    private TextField Amount;
+    @FXML
+    private Button split;
+    @FXML
+    private Text selected;
+    private String[] friends = {"Bartek", "Tomek", "Pawel", "Kuba", "Wojciech"};
+    private ObservableList<String> observableList = FXCollections.observableArrayList(friends);
+
     @FXML
     private AnchorPane splitPane;
     @FXML
@@ -21,4 +44,17 @@ public class SplitExpenseController {
         splitPane.getChildren().setAll(mainPageView);
     }
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        friendlist.setItems(observableList);
+        friendlist.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+
+    public void handleSplit(ActionEvent event){
+        StringBuilder sb = new StringBuilder();
+        for (Object o : friendlist.getSelectionModel().getSelectedItems()){
+            sb.append(o.toString());
+        }
+        selected.setText(sb.toString());
+    }
 }
